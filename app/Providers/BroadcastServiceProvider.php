7309269e93b9ba368c12be3c1bb803c37c07ba02
wkarts <57051272+wkarts@users.noreply.github.com>
2026-01-29 19:21:25 -0300

    Ajustar painel com abas analíticas e tolerância a broadcast

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -12,7 +12,9 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Broadcast::routes();
+        Broadcast::routes([
+            'middleware' => ['web', 'verificaEmpresa', 'validaAcesso'],
+        ]);
 
         require base_path('routes/channels.php');
     }
