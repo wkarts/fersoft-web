998e03599c85849d114decbd99ffed8bd28e55ba
wkarts <57051272+wkarts@users.noreply.github.com>
2026-07-23 13:14:15 -0300

    feat: adiciona cadastros rápidos em pesagens

---
+++ resources/views/pesagens/partials/cadastros-rapidos-acoes.blade.php
@@ -0,0 +1,10 @@
+<div class="btn-group ml-2 mt-1">
+    <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-plus"></i> Cadastros rápidos</button>
+    <div class="dropdown-menu">
+        <button class="dropdown-item quick-open" data-quick-type="cliente"><i class="fa fa-user mr-2"></i>Novo cliente</button>
+        <button class="dropdown-item quick-open" data-quick-type="fornecedor"><i class="fa fa-truck mr-2"></i>Novo fornecedor</button>
+        <button class="dropdown-item quick-open" data-quick-type="veiculo"><i class="fa fa-car mr-2"></i>Novo veículo</button>
+        <button class="dropdown-item quick-open" data-quick-type="funcionario"><i class="fa fa-users mr-2"></i>Novo colaborador</button>
+        <button class="dropdown-item quick-open" data-quick-type="motorista"><i class="fa fa-id-card mr-2"></i>Novo motorista</button>
+    </div>
+</div>
