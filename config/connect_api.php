738c84af72c81742b19961486c3220bb8154d96a
wkarts <57051272+wkarts@users.noreply.github.com>
2026-09-20 00:41:53 -0300

    feat(connect-api): define default ERP templates

--- config/connect_api.php
+++ config/connect_api.php
@@ -10,6 +10,33 @@ return [
     'instance_prefix' => env('CONNECT_API_INSTANCE_PREFIX', ''),
     'webhook_secret' => env('CONNECT_API_WEBHOOK_SECRET', ''),
     'webhook_url' => env('CONNECT_API_WEBHOOK_URL', ''),
+    'default_templates' => [
+        'ponto_boas_vindas' => [
+            'event_key' => 'ponto.boas_vindas',
+            'language' => 'pt_BR',
+            'body' => 'Olá, {{1}}! Sua integração de ponto está ativa. Envie OI para iniciar.',
+        ],
+        'ponto_registrado' => [
+            'event_key' => 'ponto.registro.confirmado',
+            'language' => 'pt_BR',
+            'body' => 'Olá, {{1}}! Seu ponto {{2}} foi registrado em {{3}}. Comprovante: {{4}}.',
+        ],
+        'frota_checklist' => [
+            'event_key' => 'frota.checklist',
+            'language' => 'pt_BR',
+            'body' => 'Olá, {{1}}! O checklist do veículo {{2}} está disponível em {{3}}.',
+        ],
+        'coleta_confirmada' => [
+            'event_key' => 'coleta.confirmada',
+            'language' => 'pt_BR',
+            'body' => 'Olá, {{1}}! Sua coleta foi confirmada para {{2}}.',
+        ],
+        'pesagem_concluida' => [
+            'event_key' => 'pesagem.concluida',
+            'language' => 'pt_BR',
+            'body' => 'A pesagem {{1}} foi concluída. Peso líquido: {{2}} kg.',
+        ],
+    ],
     'webhook_events' => [
         'MESSAGES_UPSERT',
         'MESSAGES_UPDATE',
