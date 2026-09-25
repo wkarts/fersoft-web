1dc7497e2eec72f6788528b55c2b3ce989f22f12
wkarts <57051272+wkarts@users.noreply.github.com>
2026-09-25 20:57:27 -0300

    fix(delivery): versionar JS do checkout automaticamente

--- resources/views/delivery/default.blade.php
+++ resources/views/delivery/default.blade.php
@@ -370,7 +370,7 @@
 	@if(isset($forma_pagamento))
 	<script type="text/javascript" src="https://stc.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js"></script>
 	<script src="/jsd/card.js" type="text/javascript"></script>
-	<script src="/jsd/forma_pagamento.js" type="text/javascript"></script>
+	<script src="/jsd/forma_pagamento.js?v={{ filemtime(public_path('jsd/forma_pagamento.js')) }}" type="text/javascript"></script>
 
 	<script type="text/javascript">
 		new Card({
